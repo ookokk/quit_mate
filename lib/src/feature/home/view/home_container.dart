51d1b6caf361ba2a3ea63a6cf92a4d2d923e89cd
osmanoscar <osmankok701@gmail.com>
2023-11-06 09:56:32 +0300

    büyüyen chart yaptım

--- lib/src/feature/home/view/home_container.dart
+++ lib/src/feature/home/view/home_container.dart
@@ -11,12 +11,13 @@ class HomeContainer extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final currentTheme = ref.watch(themeProvider);
 
-    StreamController<int> secondStreamController = StreamController<int>();
+    StreamController<double> secondStreamController =
+        StreamController<double>();
 
-    int currentSecond = 0; // Başlangıç saniyesi
+    double currentSecond = 0.0; // Başlangıç saniyesi
 
     Timer.periodic(const Duration(seconds: 1), (timer) {
-      currentSecond += 1; // Her saniyede bir saniyeyi artır
+      currentSecond += 1.0; // Her saniyede bir saniyeyi artır
       secondStreamController.sink.add(currentSecond);
     });
 
@@ -26,33 +27,38 @@ class HomeContainer extends ConsumerWidget {
         body: SizedBox(
           height: 500,
           child: Center(
-            child: StreamBuilder<int>(
+            child: StreamBuilder<double>(
               stream: secondStreamController.stream,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  int second = snapshot.data!;
+                  double second = snapshot.data!;
 
                   return SizedBox(
                     height: 300,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("Saniye: $second"),
+                        Text(
+                          "Saniye: $second",
+                          style: currentTheme.textTheme.displaySmall,
+                        ),
                         const SizedBox(height: 16),
-                        BarChart(
-                          BarChartData(
-                            titlesData: const FlTitlesData(show: false),
-                            borderData: FlBorderData(show: false),
-                            barGroups: [
-                              BarChartGroupData(x: 0, barRods: [
-                                BarChartRodData(
-                                  fromY: 0,
-                                  toY: second
-                                      .toDouble(), // Çubuğun bitiş yüksekliği
-                                  width: 16,
-                                ),
-                              ]),
-                            ],
+                        Expanded(
+                          child: BarChart(
+                            BarChartData(
+                              gridData: const FlGridData(show: false),
+                              titlesData: const FlTitlesData(show: false),
+                              borderData: FlBorderData(show: false),
+                              barGroups: [
+                                BarChartGroupData(x: 0, barRods: [
+                                  BarChartRodData(
+                                    fromY: 60 - second,
+                                    toY: 60,
+                                    width: 16,
+                                  ),
+                                ]),
+                              ],
+                            ),
                           ),
                         )
                       ],
