377de43c39d49e0b8c333e022aea4d9aa24c4c22
osmanoscar <osmankok701@gmail.com>
2023-11-06 17:15:30 +0300

    home state mixin

--- lib/src/feature/home/view/home_container.dart
+++ lib/src/feature/home/view/home_container.dart
@@ -1,51 +1,25 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quit_mate/src/core/theme/theme_provider.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:quit_mate/src/feature/home/viewmodel/home_state_mixin.dart';
 import 'package:quit_mate/src/product/user/model/sober_user.dart';
 import 'package:quit_mate/src/product/user/repository/user_repository.dart';
 
-class HomeContainer extends ConsumerWidget {
+class HomeContainer extends ConsumerStatefulWidget {
   const HomeContainer({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeContainer> createState() => _HomeContainerState();
+}
+
+class _HomeContainerState extends ConsumerState<HomeContainer>
+    with HomeStateMixin {
+  @override
+  Widget build(BuildContext context) {
     final currentTheme = ref.watch(themeProvider);
-
-    StreamController<Map<String, double>> dataStreamController =
-        StreamController<Map<String, double>>();
-
-    double currentSecond = 50.0;
-    double currentMinute = 10.0;
-    double currentHour = 10.0;
-    double currentDay = 10.0;
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      currentSecond += 1.0;
-      if (currentSecond >= 60) {
-        currentSecond = 0.0;
-        currentMinute += 1.0;
-      }
-
-      if (currentMinute >= 60) {
-        currentMinute = 0.0;
-        currentHour += 1.0;
-      }
-
-      if (currentHour >= 24) {
-        currentHour = 0.0;
-        currentDay += 1.0;
-      }
-
-      dataStreamController.sink.add({
-        'second': currentSecond,
-        'minute': currentMinute,
-        'hour': currentHour,
-        'day': currentDay,
-      });
-    });
     final UserRepository userRepository = UserRepository();
-    final String userId = "user123";
+    final String userId = "user1234";
 
     return SafeArea(
       child: Scaffold(
@@ -77,44 +51,8 @@ class HomeContainer extends ConsumerWidget {
                       },
                       icon: const Icon(Icons.plus_one_outlined),
                     ),
-                    IconButton(
-                      onPressed: () async {
-                        // Kullanıcı verilerini kaydet
-                        final user = SoberUser(
-                          addictiveFactor: "Example",
-                          soberStartDate: DateTime.now(),
-                          weeklyUse: 3,
-                          dailyUseOnDays: 2,
-                          pledgeTime: const TimeOfDay(hour: 12, minute: 12),
-                          reviewTime: const TimeOfDay(hour: 11, minute: 11),
-                        );
-                        await userRepository.saveUser(userId, user);
-                        print("Kullanıcı verileri kaydedildi.");
-                      },
-                      icon: Text(
-                        'Set',
-                        style: currentTheme.textTheme.bodyLarge,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () async {
-                        // Kullanıcı verilerini al
-                        final retrievedUser =
-                            await userRepository.getUser(userId);
-                        if (retrievedUser != null) {
-                          print(
-                              "Kullanıcı verileri alındı: ${retrievedUser.toJson()}");
-                        } else {
-                          print("Kullanıcı bulunamadı.");
-                        }
-                      },
-                      icon: Text(
-                        'Get',
-                        style: currentTheme.textTheme.bodyLarge,
-                      ),
-                    ),
                     const SizedBox(height: 16),
-                    /*Expanded(
+                    Expanded(
                       child: BarChart(
                         BarChartData(
                           gridData: const FlGridData(show: true),
@@ -152,7 +90,7 @@ class HomeContainer extends ConsumerWidget {
                           ],
                         ),
                       ),
-                    ),*/
+                    ),
                   ],
                 ),
               );
---
+++ lib/src/feature/home/viewmodel/home_state_mixin.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quit_mate/src/feature/home/view/home_container.dart';
+
+mixin HomeStateMixin on ConsumerState<HomeContainer> {
+  double currentSecond = 50.0;
+  double currentMinute = 10.0;
+  double currentHour = 10.0;
+  double currentDay = 10.0;
+  final StreamController<Map<String, double>> dataStreamController =
+      StreamController<Map<String, double>>();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      currentSecond += 1.0;
+      if (currentSecond >= 60) {
+        currentSecond = 0.0;
+        currentMinute += 1.0;
+      }
+
+      if (currentMinute >= 60) {
+        currentMinute = 0.0;
+        currentHour += 1.0;
+      }
+
+      if (currentHour >= 24) {
+        currentHour = 0.0;
+        currentDay += 1.0;
+      }
+
+      dataStreamController.sink.add({
+        'second': currentSecond,
+        'minute': currentMinute,
+        'hour': currentHour,
+        'day': currentDay,
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    dataStreamController.close();
+    super.dispose();
+  }
+}
