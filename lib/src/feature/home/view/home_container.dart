242a7a6be6cfe89860bffc2adba5c9efb1897ddb
osmanoscar <osmankok701@gmail.com>
2023-11-08 10:11:36 +0300

    scaffoldu sildim

--- lib/src/feature/home/view/home_container.dart
+++ lib/src/feature/home/view/home_container.dart
@@ -18,81 +18,80 @@ class _HomeContainerState extends ConsumerState<HomeContainer>
   Widget build(BuildContext context) {
     final currentTheme = ref.watch(themeProvider);
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: currentTheme.scaffoldBackgroundColor,
-        body: SizedBox(
-          height: 700,
-          child: StreamBuilder<Map<String, double>>(
-            stream: dataStreamController.stream,
-            builder: (context, snapshot) {
-              final data = snapshot.data;
-              if (data == null) {
-                return const SizedBox(
-                    height: 50, width: 50, child: CircularProgressIndicator());
-              }
-              final currentSecond = data['second'] ?? 0.0;
-              final currentMinute = data['minute'] ?? 0.0;
-              final currentHour = data['hour'] ?? 0.0;
-              final currentDay = data['day'] ?? 0.0;
-
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                        onPressed: () async {
-                          print(currentDay);
-                          print(currentHour);
-                          print(currentMinute);
-                          print(currentSecond);
-                        },
-                        child: Text('check')),
-                    Expanded(
-                      child: BarChart(
-                        BarChartData(
-                          gridData: const FlGridData(show: true),
-                          titlesData: const FlTitlesData(show: true),
-                          borderData: FlBorderData(show: true),
-                          barGroups: [
-                            BarChartGroupData(x: 0, barRods: [
-                              BarChartRodData(
-                                fromY: 60 - currentDay,
-                                toY: 60,
-                                width: 16,
-                              ),
-                            ]),
-                            BarChartGroupData(x: 1, barRods: [
-                              BarChartRodData(
-                                fromY: 60 - currentHour,
-                                toY: 60,
-                                width: 16,
-                              ),
-                            ]),
-                            BarChartGroupData(x: 2, barRods: [
-                              BarChartRodData(
-                                fromY: 60 - currentMinute,
-                                toY: 60,
-                                width: 16,
-                              ),
-                            ]),
-                            BarChartGroupData(x: 3, barRods: [
-                              BarChartRodData(
-                                fromY: 60 - currentSecond,
-                                toY: 60,
-                                width: 16,
-                              ),
-                            ]),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+        child: Container(
+      color: currentTheme.scaffoldBackgroundColor,
+      height: 400,
+      child: StreamBuilder<Map<String, double>>(
+        stream: dataStreamController.stream,
+        builder: (context, snapshot) {
+          final data = snapshot.data;
+          if (data == null) {
+            return const SizedBox(
+                height: 50, width: 50, child: CircularProgressIndicator());
+          }
+          final currentSecond = data['second'] ?? 0.0;
+          final currentMinute = data['minute'] ?? 0.0;
+          final currentHour = data['hour'] ?? 0.0;
+          final currentDay = data['day'] ?? 0.0;
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: () async {
+                    print(currentDay);
+                    print(currentHour);
+                    print(currentMinute);
+                    print(currentSecond);
+                  },
+                  child: Text('check')),
+              SizedBox(
+                height: 400,
+                child: BarChart(
+                  swapAnimationDuration: const Duration(milliseconds: 300),
+                  BarChartData(
+                    alignment: BarChartAlignment.spaceAround,
+                    maxY: 60,
+                    minY: 0,
+                    gridData: const FlGridData(show: true),
+                    titlesData: const FlTitlesData(show: true),
+                    borderData: FlBorderData(show: true),
+                    barGroups: [
+                      BarChartGroupData(x: 0, barRods: [
+                        BarChartRodData(
+                            fromY: 60 - currentDay,
+                            toY: 60,
+                            width: 35,
+                            borderRadius: BorderRadius.circular(5)),
+                      ]),
+                      BarChartGroupData(x: 1, barRods: [
+                        BarChartRodData(
+                            fromY: 60 - currentHour,
+                            toY: 60,
+                            width: 35,
+                            borderRadius: BorderRadius.circular(5)),
+                      ]),
+                      BarChartGroupData(x: 2, barRods: [
+                        BarChartRodData(
+                            fromY: 60 - currentMinute,
+                            toY: 60,
+                            width: 35,
+                            borderRadius: BorderRadius.circular(5)),
+                      ]),
+                      BarChartGroupData(x: 3, barRods: [
+                        BarChartRodData(
+                            fromY: 60 - currentSecond,
+                            toY: 60,
+                            width: 35,
+                            borderRadius: BorderRadius.circular(5)),
+                      ]),
+                    ],
+                  ),
                 ),
-              );
-            },
-          ),
-        ),
+              )
+            ],
+          );
+        },
       ),
-    );
+    ));
   }
 }
