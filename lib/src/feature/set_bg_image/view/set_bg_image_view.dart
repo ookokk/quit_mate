a2bf115db87b40ba63e87035c1ac7d6513dc4621
osmanoscar <osmankok701@gmail.com>
2023-11-09 12:10:40 +0300

    deliye dönmeme sebep

--- lib/src/feature/set_bg_image/view/set_bg_image_view.dart
+++ lib/src/feature/set_bg_image/view/set_bg_image_view.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quit_mate/generated/assets.dart';
 import 'package:quit_mate/src/core/cache/sp_background_image/sp_background_image.dart';
+import 'package:quit_mate/src/core/const/material/device_size.dart';
 import 'package:quit_mate/src/core/theme/theme_provider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class SetBGImageView extends ConsumerStatefulWidget {
   const SetBGImageView({
@@ -14,6 +16,7 @@ class SetBGImageView extends ConsumerStatefulWidget {
 }
 
 class SetBGImageViewState extends ConsumerState<SetBGImageView> {
+  final PageController _pageController = PageController();
   final backgroundImages = [
     Assets.imagesWp1,
     Assets.imagesWp2,
@@ -33,40 +36,108 @@ class SetBGImageViewState extends ConsumerState<SetBGImageView> {
     final sharedBGManager = SPBackgroundImage();
     return SafeArea(
       child: Scaffold(
-        backgroundColor: currentTheme.scaffoldBackgroundColor,
-        body: Column(
-          children: [
-            SizedBox(
-              height: 500,
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 9 / 16,
+        backgroundColor: currentTheme.hoverColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: DeviceSize.kHeight(context) * 0.7,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: backgroundImages.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(36),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.6,
+                            decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.grey, width: 0.5),
+                              image: DecorationImage(
+                                image: AssetImage(backgroundImages[index]),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
                 ),
-                itemCount: backgroundImages.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () async {
-                      await sharedBGManager
-                          .setNewBackgroundImage(backgroundImages[index]);
-                      setState(() {});
-                    },
-                    child: Image.asset(
-                      backgroundImages[index],
-                      fit: BoxFit.cover,
-                    ),
-                  );
-                },
               ),
-            ),
-            TextButton(
+              pageIndicatorAndArrowButtonsRow(currentTheme),
+              ElevatedButton(
+                onPressed: () async {
+                  final int currentIndex = _pageController.page?.round() ?? 0;
+                  await sharedBGManager
+                      .setNewBackgroundImage(backgroundImages[currentIndex]);
+                  setState(() {});
+                },
+                child: const Text('Resmi Arkaplan Yap'),
+              ),
+              ElevatedButton(
                 onPressed: () async {
                   final ahmet = await sharedBGManager.getBackgroundImage();
                   print(ahmet);
+                  setState(() {});
                 },
-                child: Text('CHECK')),
-          ],
+                child: const Text('Kontrol'),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Padding pageIndicatorAndArrowButtonsRow(ThemeData currentTheme) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 22, right: 22),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {
+              if (_pageController.page != null && _pageController.page! > 0) {
+                _pageController.previousPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              }
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: currentTheme.indicatorColor,
+            ),
+          ),
+          SmoothPageIndicator(
+            controller: _pageController,
+            count: backgroundImages.length,
+            effect: WormEffect(
+                activeDotColor: currentTheme.primaryColor,
+                paintStyle: PaintingStyle.stroke,
+                dotHeight: 10,
+                dotWidth: 10),
+          ),
+          IconButton(
+            onPressed: () {
+              if (_pageController.page != null &&
+                  _pageController.page! < backgroundImages.length - 1) {
+                _pageController.nextPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              }
+            },
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              color: currentTheme.indicatorColor,
+            ),
+          ),
+        ],
       ),
     );
   }
