1d9c53a60b05c4d5e4c5a39524873939677c47da
osmanoscar <osmankok701@gmail.com>
2023-11-12 15:50:28 +0300

    sized box u extract yaptım

--- lib/src/feature/settings/feature/privacy/view/privacy_view.dart
+++ lib/src/feature/settings/feature/privacy/view/privacy_view.dart
@@ -29,49 +29,35 @@ class PrivacyView extends ConsumerWidget {
                 Strings.bLastUpdated,
                 style: currentTheme.textTheme.bodyMedium,
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.bYourSecurity,
                 style: currentTheme.textTheme.bodyMedium,
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.bWe,
                 style: currentTheme.textTheme.bodyMedium,
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.tYourPersonal,
                 style: currentTheme.textTheme.bodyLarge
                     ?.copyWith(fontWeight: FontWeight.w600),
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(Strings.bYourIdentity,
                   style: currentTheme.textTheme.bodyMedium),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.tPurpose,
                 style: currentTheme.textTheme.bodyLarge
                     ?.copyWith(fontWeight: FontWeight.w600),
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(Strings.bYourPurpose,
                   style: currentTheme.textTheme.bodyMedium),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.tToWhom,
                 style: currentTheme.textTheme.bodyLarge
@@ -81,39 +67,44 @@ class PrivacyView extends ConsumerWidget {
                 height: 6,
               ),
               Text(Strings.bToWhom, style: currentTheme.textTheme.bodyMedium),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.tYourRights,
                 style: currentTheme.textTheme.bodyLarge
                     ?.copyWith(fontWeight: FontWeight.w600),
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(Strings.bPursuant, style: currentTheme.textTheme.bodyMedium),
               Text(Strings.bToLearnA, style: currentTheme.textTheme.bodyMedium),
               Text(Strings.bToLearnB, style: currentTheme.textTheme.bodyMedium),
               Text(Strings.bToLearnC, style: currentTheme.textTheme.bodyMedium),
               Text(Strings.bToLearnD, style: currentTheme.textTheme.bodyMedium),
               Text(Strings.bToLearnE, style: currentTheme.textTheme.bodyMedium),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(
                 Strings.tContact,
                 style: currentTheme.textTheme.bodyLarge
                     ?.copyWith(fontWeight: FontWeight.w600),
               ),
-              const SizedBox(
-                height: 6,
-              ),
+              const CustomSizedBox(),
               Text(Strings.bInOrder, style: currentTheme.textTheme.bodyMedium),
             ],
           ),
         ),
       ),
     ));
+  }
+}
+
+class CustomSizedBox extends StatelessWidget {
+  const CustomSizedBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 6,
+    );
   }
 }
