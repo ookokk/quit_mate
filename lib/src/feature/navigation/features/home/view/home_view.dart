00ed6b43fab53ac7224c34592804d63583ee3cbd
osmanoscar <osmankok701@gmail.com>
2023-12-21 12:40:01 +0300

    ad mob view ekledim ama denemedim

--- lib/src/feature/navigation/features/home/view/home_view.dart
+++ lib/src/feature/navigation/features/home/view/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:quit_mate/src/core/const/material/device_size.dart';
 import 'package:quit_mate/src/core/theme/theme_provider.dart';
 import 'package:quit_mate/src/feature/navigation/features/progress/view/sober_page_view.dart';
@@ -16,6 +17,7 @@ class HomeView extends ConsumerWidget {
     final currentTheme = ref.watch(themeProvider);
     final soberProvider = ref.watch(soberChartProvider);
     final totalDaysSober = soberProvider.calculateTotalDaysSober();
+    BannerAd? bannerAd;
     return SafeArea(
         child: Scaffold(
       backgroundColor: currentTheme.scaffoldBackgroundColor,
@@ -26,6 +28,10 @@ class HomeView extends ConsumerWidget {
             const SizedBox(
               height: 8,
             ),
+            /*  SizedBox(
+                height: bannerAd!.size.height.toDouble(),
+                width: bannerAd.size.width.toDouble(),
+                child: AdWidget(ad: bannerAd)),*/
             Padding(
               padding: const EdgeInsets.only(left: 12),
               child: Align(
