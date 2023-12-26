82f949491e8a3a22ff3cde471645ebe149810453
osmanoscar <osmankok701@gmail.com>
2023-12-26 09:21:43 +0300

    easy localization u sildim main darttan

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quit_mate/generated/codegen_loader.g.dart';
 import 'package:quit_mate/src/core/const/routes.dart';
-import 'package:quit_mate/src/core/language/language_manager.dart';
 import 'package:quit_mate/src/core/theme/theme_provider.dart';
 import 'package:quit_mate/src/feature/splash/splash/view/splash_view.dart';
 import 'src/core/init/init_app.dart';
@@ -11,13 +9,7 @@ import 'src/core/init/init_app.dart';
 void main() async {
   InitApp().initApp();
   EasyLocalization.logger.enableBuildModes = [];
-  runApp(EasyLocalization(
-      supportedLocales: LanguageManager.instance.supportedLocales,
-      startLocale: LanguageManager.instance.currentLocale,
-      path: 'assets/translations',
-      fallbackLocale: const Locale('en', 'US'),
-      assetLoader: const CodegenLoader(),
-      child: const ProviderScope(child: MyApp())));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
@@ -27,9 +19,6 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final currentTheme = ref.watch(themeProvider);
     return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: LanguageManager.instance.currentLocale,
       theme: currentTheme,
       routes: Routes.routes,
       debugShowCheckedModeBanner: false,
@@ -38,4 +27,4 @@ class MyApp extends ConsumerWidget {
   }
 }
 //gitignore düzenlemek lazım
-// flutter build appbundle --build-name=0.0.1 --build-number=2
+// flutter build appbundle --build-name=0.0.1 --build-number=4
--- lib/src/core/init/init_app.dart
+++ lib/src/core/init/init_app.dart
@@ -1,6 +1,6 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:quit_mate/src/core/network/firebase_options.dart';
 
@@ -11,8 +11,6 @@ class InitApp {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    await EasyLocalization.ensureInitialized();
-    //await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    EasyLocalization.logger.enableBuildModes = [];
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   }
 }
