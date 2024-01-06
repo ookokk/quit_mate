32e88c01d52b00b431288d3d7b1983f100b817bb
osmanoscar <osmankok701@gmail.com>
2024-01-06 11:01:13 +0300

    test  import bozulmus

--- test/features/affirmation/affirmation_test.dart
+++ test/features/affirmation/affirmation_test.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quit_mate/src/feature/navigation/features/affirmation/model/get_random_affirmation.dart';
+
 void main() {
   // All tests passed!
   test('GetRandomAffirmation returns a non-null affirmation', () {
