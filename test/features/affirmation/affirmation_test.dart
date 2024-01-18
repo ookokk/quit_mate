365cdd47ed24130c9335dca830246ade32d521bb
Osman kök <114760131+ookokk@users.noreply.github.com>
2024-01-18 22:10:57 +0300

    Update affirmation_test.dart

--- test/features/affirmation/affirmation_test.dart
+++ test/features/affirmation/affirmation_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:quit_mate/src/feature/navigation/features/affirmation/model/get_random_affirmation.dart';
 
 void main() {
-  // All tests passed!
   test('GetRandomAffirmation returns a non-null affirmation', () {
     final affirmationGenerator = GetRandomAffirmation();
     final affirmation = affirmationGenerator.getRandomAffirmation();
